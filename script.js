eba0236980c97fc377cea94b05346ac691fdbfb5
Jompa112 <nilssonjohannes43@gmail.com>
2025-02-27 21:56:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const cars = [
+    { name: "BMW M5", price: "500,000", image: "https://via.placeholder.com/250" },
+    { name: "Audi RS6", price: "600,000", image: "https://via.placeholder.com/250" },
+    { name: "Mercedes AMG GT", price: "700,000", image: "https://via.placeholder.com/250" }
+];
